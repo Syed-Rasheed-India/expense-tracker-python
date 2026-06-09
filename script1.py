c05f461ae944a6d8c343f4d37de9844b9f50ca13
Syed Rasheed <syedrasheedindia@gmail.com>
2026-06-09 21:15:38 +0530

    first commit

--- script1.py
+++ script1.py
@@ -1,23 +1,57 @@
-#Student Grade Calculator
+amount = 0
 
-try:
-    mark = int(input("Enter mark: "))
+def balance():
+    print("Balance =", amount)
 
-except Exception as e:
-    print(e)
+def deposit(amt):
+    global amount
+    amount += amt
 
-else:
-    if mark >= 90:
-        print("Your Grade Is O")
-    elif mark >= 80:
-        print("Your Grade Is A")
-    elif mark >= 70:
-        print("Your Grade Is B")
-    elif mark >= 60:
-        print("Your Grade Is C")
-    elif mark >= 50:
-        print("Your Grade Is D")
-    elif mark <=20:
-        print("Your Grade Is F")
-    else:
-        print("Your Grade Is E")
+def withdraw(amt):
+    global amount
+    amount -= amt
+
+while True:
+    print("1.Check Balance")
+    print("2.Deposit Money")
+    print("3.Withdraw Money")
+    print("4.Exit")
+
+    try:
+        option = int(input("Enter Your Option : "))
+    except ValueError:
+        print("Please enter a valid option")
+        continue
+
+    match option:
+        case 1:
+            balance()
+
+        case 2:
+            try:
+                deposit_amount = int(input("Enter Your Deposit Amount : "))
+                if deposit_amount > 0:
+                    deposit(deposit_amount)
+                    print("Deposit Successful")
+                else:
+                    print("Amount should be greater than 0")
+            except ValueError:
+                print("Please enter a valid number")
+
+        case 3:
+            try:
+                withdraw_amount = int(input("Enter Your Withdraw Amount : "))
+                if withdraw_amount > 0:
+                    withdraw(withdraw_amount)
+                    print("Withdraw Successful")
+                else:
+                    print("Amount should be greater than 0")
+            except ValueError:
+                print("Please enter a valid number")
+
+        case 4:
+            print("Program Ended")
+            break
+
+        case _:
+            print("Invalid Option")
