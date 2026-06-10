d92a93cf2e76cb3b5cd1cfc1733b44491de5c421
Syed Rasheed <syedrasheedindia@gmail.com>
2026-06-10 13:42:03 +0530

    first time

--- script1.py
+++ script1.py
@@ -1,57 +1,29 @@
-amount = 0
-
-def balance():
-    print("Balance =", amount)
-
-def deposit(amt):
-    global amount
-    amount += amt
-
-def withdraw(amt):
-    global amount
-    amount -= amt
+import random
 
 while True:
-    print("1.Check Balance")
-    print("2.Deposit Money")
-    print("3.Withdraw Money")
-    print("4.Exit")
+    print("1. Start")
+    print("2. End")
+    option = int(input("Enter a Option: "))
+    if option == 2:
+        break
+    else:
+        random_number = random.randint(1, 10)
+        attempt=0
+        while True:
+            number = int(input("Enter a Number From 1 to 10: "))
+            if number == random_number:
+                # global attempt
+                print("You Won! you Won by ",attempt,"Attempts")
+                break
+            elif number > random_number:
+                # global attempt
+                attempt+=1
+                print("your Number is Higher !! Try Again")
+            elif number < random_number:
+                # global attempt
+                attempt+=1
+                print("your Number is Lower !! Try Again")
 
-    try:
-        option = int(input("Enter Your Option : "))
-    except ValueError:
-        print("Please enter a valid option")
-        continue
 
-    match option:
-        case 1:
-            balance()
 
-        case 2:
-            try:
-                deposit_amount = int(input("Enter Your Deposit Amount : "))
-                if deposit_amount > 0:
-                    deposit(deposit_amount)
-                    print("Deposit Successful")
-                else:
-                    print("Amount should be greater than 0")
-            except ValueError:
-                print("Please enter a valid number")
 
-        case 3:
-            try:
-                withdraw_amount = int(input("Enter Your Withdraw Amount : "))
-                if withdraw_amount > 0:
-                    withdraw(withdraw_amount)
-                    print("Withdraw Successful")
-                else:
-                    print("Amount should be greater than 0")
-            except ValueError:
-                print("Please enter a valid number")
-
-        case 4:
-            print("Program Ended")
-            break
-
-        case _:
-            print("Invalid Option")
